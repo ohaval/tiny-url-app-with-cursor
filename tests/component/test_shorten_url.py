3ceb5c4774316fb373d240902c5c623dd2b672df
Ohav <ohav@jit.io>
2025-03-30 19:34:57 +0300

    Add mock aws_credentials

--- tests/component/test_shorten_url.py
+++ tests/component/test_shorten_url.py
@@ -13,6 +13,16 @@ from src.handlers.shorten_url import dynamo_ops, handler
 os.environ["BASE_URL"] = "https://tiny.url"
 
 
+@pytest.fixture(scope="session")
+def aws_credentials():
+    """Mocked AWS Credentials for boto3."""
+    os.environ['AWS_ACCESS_KEY_ID'] = 'testing'
+    os.environ['AWS_SECRET_ACCESS_KEY'] = 'testing'
+    os.environ['AWS_SECURITY_TOKEN'] = 'testing'
+    os.environ['AWS_SESSION_TOKEN'] = 'testing'
+    os.environ['AWS_DEFAULT_REGION'] = 'us-east-1'
+
+
 @pytest.fixture
 def dynamodb_table() -> Generator[Any, None, None]:
     """Set up DynamoDB test table."""
